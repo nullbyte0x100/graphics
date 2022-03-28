9c36068047d8b909c4e58f19bc9e03b4d03540e1
Malesi-Trevor-Git <trevormalesi4@gmail.com>
2022-03-28 12:47:31 +0300

    Go

--- mouse.cpp
+++ mouse.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <vector>
+#include <string>
+#include<numeric>
 class CodeWars{
 public:
 static int divisors(int n){
@@ -12,6 +15,13 @@ static int divisors(int n){
     }
     return count;
 }
+static std::string odd_or_even(const std::vector<int> &arr){
+    return std::accumulate(arr.begin(),arr.end(),0)&1?"odd":"even";
+}
+static std::vector<int> evenNumbers(std::vector<int>arr,int n){
+
+}
 };
 int main(){
+
 }
