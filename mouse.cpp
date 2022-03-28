2a7af2e0921bcd579c45f8945b5f687ba61bdcb1
Malesi-Trevor-Git <trevormalesi4@gmail.com>
2022-03-28 12:38:12 +0300

    divisors

--- mouse.cpp
+++ mouse.cpp
@@ -1,5 +1,17 @@
-#include<iostream>
-#include <shader-malesi.h>
-int main(int argc,char **argv){
-    std::cout<<"Check"<<"\n";
+#include <iostream>
+class CodeWars{
+public:
+static int divisors(int n){
+    int count{};
+    for (int i=1;i<=n;i++){
+        if (n%i==0){
+            count++;
+        }else{
+
+        }
+    }
+    return count;
+}
+};
+int main(){
 }
